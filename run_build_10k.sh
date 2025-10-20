f680d362be7dbc700a019bc44846b89ebc8ef5be
Philipp Kempski <pkempski@login03.frontier.olcf.ornl.gov>
2025-10-20 10:47:17 -0400

    Clean up

--- run_build_10k.sh
+++ run_build_10k.sh
@@ -23,6 +23,6 @@ cmake -Bbuild -DAthena_ENABLE_MPI=ON -DKokkos_ARCH_ZEN3=ON -DKokkos_ARCH_VEGA90A
       -DKokkos_ENABLE_HIP=ON -DCMAKE_CXX_COMPILER=CC \
       -DCMAKE_EXE_LINKER_FLAGS="-L${ROCM_PATH}/lib -lamdhip64" \
       -DCMAKE_CXX_FLAGS="-I${ROCM_PATH}/include  -munsafe-fp-atomics" \
-      -DPROBLEM=turb # part_static_turb
+      -DPROBLEM=part_static_turb
 cd ${build}
 make -j
